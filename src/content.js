de6fd1422b69054b8c20dccd406cc6df72a65c5b
liyang <1711467488@qq.com>
2018-11-20 19:58:06 +0800

    listener

--- src/content.js
+++ src/content.js
@@ -1,12 +1,2 @@
-import crawler from './crawler'
 import './submit'
-
-// 响应 background 信息
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendMessage) {
-    // 获取页面信息
-    if (request.action == 'getInfo') {
-      sendMessage(crawler())
-    }
-  }
-)
+import './listener'
---
+++ src/listener.js
@@ -0,0 +1,11 @@
+import crawler from './crawler'
+
+// 响应 background 信息
+chrome.extension.onMessage.addListener(
+  function(request, sender, sendMessage) {
+    // 获取页面信息
+    if (request.action == 'getInfo') {
+      sendMessage(crawler())
+    }
+  }
+)
