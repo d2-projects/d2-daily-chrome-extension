a011f4fa2d2fae01246f3f3b0e261a9885f3ec82
liyang <1711467488@qq.com>
2018-12-21 14:37:24 +0800

    no message

--- src/content.js
+++ src/content.js
@@ -41,7 +41,7 @@ chrome.extension.onMessage.addListener(
           <p style="margin: 0; text-align: left; font-size: 14px; font-weight: bold;">简介</p>
           <textarea id="d2-daily-description" class="swal2-textarea" style="margin: 10px 0;" row="2">${data.description}</textarea>
           <p style="margin: 0; text-align: left; font-size: 14px; font-weight: bold;">
-            <input type="checkbox" id="network-low"> 不易访问的网络环境，需要特殊处理
+            <input type="checkbox" id="network-low"> 不易访问的网站，需要特殊处理
           </p>
           <p style="margin: 0; text-align: left; font-size: 14px; font-weight: bold;">
             <input type="checkbox" id="is-video"> 视频类型的内容
--- src/crawler.js
+++ src/crawler.js
@@ -16,15 +16,17 @@ export default function crawler () {
   function getGithubInfo () {
     if (isGithub()) {
       // 语言
-      githubInfo.lang = (document.querySelector('#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > button').innerText || '')
+      const lang = (document.querySelector('#js-repo-pjax-container > div.container.new-discussion-timeline.experiment-repo-nav > div.repository-content > button').innerText || '')
         .split('\n')
         .filter(e => e !== '')
+      githubInfo.lang = lang.join(',')
+      githubInfo.langPrimary = lang[0] || ''
       // watch
-      githubInfo.watch = document.querySelector('.pagehead-actions li:nth-child(1) form:nth-child(1) a').innerText
+      githubInfo.watch = Number(document.querySelector('.pagehead-actions li:nth-child(1) form:nth-child(1) a').innerText)
       // star
-      githubInfo.star = document.querySelector('.pagehead-actions li:nth-child(2) form:nth-child(1) a').innerText.trim().replace(',', '')
+      githubInfo.star = Number(document.querySelector('.pagehead-actions li:nth-child(2) form:nth-child(1) a').innerText.trim().replace(',', ''))
       // fork
-      githubInfo.fork = document.querySelector('.pagehead-actions li:nth-child(3) a.social-count').innerText
+      githubInfo.fork = Number(document.querySelector('.pagehead-actions li:nth-child(3) a.social-count').innerText)
     }
   }
 
@@ -34,10 +36,11 @@ export default function crawler () {
   let description = document.title
   // 如果是 github 开源仓库 获取 github 的数据
   let githubInfo = {
-    lang: [],
-    watch: '',
-    star: '',
-    fork: ''
+    lang: '',
+    langPrimary: '',
+    watch: null,
+    star: null,
+    fork: null
   }
 
   switch (parsedUrl.host) {
