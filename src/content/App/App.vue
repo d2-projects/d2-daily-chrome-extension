92dcd2e2ec09547cdfdfc7067fa8b82e88ebd3c4
liyang <1711467488@qq.com>
2019-05-30 14:42:58 +0800

    :construction: Temporary preservation

---
+++ src/content/App/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="main_app">
+    <h1>欢迎使用日报提交工具</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    alert (msg) {
+      alert(msg)
+    }
+  }
+}
+</script>
+
+<style>
+.main_app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/content/index.js
+++ src/content/index.js
@@ -1,17 +1,16 @@
 /* eslint-disable */
-
-import {
-  Message
-} from 'element-ui'
-
+import Vue from 'vue'
 import './tools/insert-element-icons'
+import App from './App/App.vue'
+
+const AppConstructor = Vue.extend(App)
+const instance = new AppConstructor()
+instance.$mount();
+document.body.appendChild(instance.$el);
 
 chrome.extension.onMessage.addListener(
   async function (request, sender, sendMessage) {
     console.log('request.action', request.action)
     console.log('request.category', request.category)
-    Message.success(`${request.action} ${request.category}`)
   }
 )
-
-console.log('Hello')
