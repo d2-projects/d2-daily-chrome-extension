52d52d73a86106484177a054d6def9906b650630
liyang <1711467488@qq.com>
2019-05-30 13:33:36 +0800

    :construction: Temporary preservation

--- src/content/index.js
+++ src/content/index.js
@@ -2,15 +2,18 @@ import {
   Message
 } from 'element-ui'
 
-// 发现element的字体文件无法通过打包加载，所以另外通过cdn来加载样式
-let elementCss = document.createElement('link')
-elementCss.href = 'https://unpkg.com/element-ui@2.8.2/lib/theme-chalk/index.css'
-elementCss.rel = 'stylesheet'
-document.head.append(elementCss)
-
-// Message({
-//   type: 'info',
-//   message: '你好'
-// })
+// 通过Chrome插件的API加载字体文件
+(function insertElementIcons() {
+  let elementIcons = document.createElement('style')
+  elementIcons.type = 'text/css'
+  elementIcons.textContent = `
+      @font-face {
+          font-family: "element-icons";
+          src: url('${ window.chrome.extension.getURL('fonts/element-icons.woff')}') format('woff'),
+          url('${ window.chrome.extension.getURL('fonts/element-icons.ttf ')}') format('truetype'); /* chrome, firefox, opera, Safari, Android, iOS 4.2+*/
+      }
+  `
+  document.head.appendChild(elementIcons)
+})()
 
 console.log('Hello')
